3c6535b6179710bbc2395f8db6555ea9db491078
yuvalmaimon3 <54181113+yuvalmaimon3@users.noreply.github.com>
2019-08-17 00:23:34 +0300

    my project

---
+++ Windows Form XO/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.window1 = new System.Windows.Forms.Button();
+            this.window3 = new System.Windows.Forms.Button();
+            this.window2 = new System.Windows.Forms.Button();
+            this.window5 = new System.Windows.Forms.Button();
+            this.window6 = new System.Windows.Forms.Button();
+            this.window4 = new System.Windows.Forms.Button();
+            this.window8 = new System.Windows.Forms.Button();
+            this.window9 = new System.Windows.Forms.Button();
+            this.window7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // window1
+            // 
+            this.window1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.window1.Location = new System.Drawing.Point(33, 29);
+            this.window1.Name = "window1";
+            this.window1.Size = new System.Drawing.Size(75, 70);
+            this.window1.TabIndex = 0;
+            this.window1.Text = "1";
+            this.window1.UseVisualStyleBackColor = true;
+            this.window1.Click += new System.EventHandler(this.Mark);
+            // 
+            // window3
+            // 
+            this.window3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.window3.Location = new System.Drawing.Point(267, 29);
+            this.window3.Name = "window3";
+            this.window3.Size = new System.Drawing.Size(75, 70);
+            this.window3.TabIndex = 1;
+            this.window3.Text = "3";
+            this.window3.UseVisualStyleBackColor = true;
+            this.window3.Click += new System.EventHandler(this.Mark);
+            // 
+            // window2
+            // 
+            this.window2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.window2.Location = new System.Drawing.Point(150, 29);
+            this.window2.Name = "window2";
+            this.window2.Size = new System.Drawing.Size(75, 70);
+            this.window2.TabIndex = 2;
+            this.window2.Text = "2";
+            this.window2.UseVisualStyleBackColor = true;
+            this.window2.Click += new System.EventHandler(this.Mark);
+            // 
+            // window5
+            // 
+            this.window5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.window5.Location = new System.Drawing.Point(150, 105);
+            this.window5.Name = "window5";
+            this.window5.Size = new System.Drawing.Size(75, 70);
+            this.window5.TabIndex = 5;
+            this.window5.Text = "5";
+            this.window5.UseVisualStyleBackColor = true;
+            this.window5.Click += new System.EventHandler(this.Mark);
+            // 
+            // window6
+            // 
+            this.window6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.window6.Location = new System.Drawing.Point(267, 105);
+            this.window6.Name = "window6";
+            this.window6.Size = new System.Drawing.Size(75, 70);
+            this.window6.TabIndex = 4;
+            this.window6.Text = "6";
+            this.window6.UseVisualStyleBackColor = true;
+            this.window6.Click += new System.EventHandler(this.Mark);
+            // 
+            // window4
+            // 
+            this.window4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.window4.Location = new System.Drawing.Point(33, 105);
+            this.window4.Name = "window4";
+            this.window4.Size = new System.Drawing.Size(75, 70);
+            this.window4.TabIndex = 3;
+            this.window4.Text = "4";
+            this.window4.UseVisualStyleBackColor = true;
+            this.window4.Click += new System.EventHandler(this.Mark);
+            // 
+            // window8
+            // 
+            this.window8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.window8.Location = new System.Drawing.Point(150, 181);
+            this.window8.Name = "window8";
+            this.window8.Size = new System.Drawing.Size(75, 70);
+            this.window8.TabIndex = 8;
+            this.window8.Text = "8";
+            this.window8.UseVisualStyleBackColor = true;
+            this.window8.Click += new System.EventHandler(this.Mark);
+            // 
+            // window9
+            // 
+            this.window9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.window9.Location = new System.Drawing.Point(267, 181);
+            this.window9.Name = "window9";
+            this.window9.Size = new System.Drawing.Size(75, 70);
+            this.window9.TabIndex = 7;
+            this.window9.Text = "9";
+            this.window9.UseVisualStyleBackColor = true;
+            this.window9.Click += new System.EventHandler(this.Mark);
+            // 
+            // window7
+            // 
+            this.window7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.window7.Location = new System.Drawing.Point(33, 181);
+            this.window7.Name = "window7";
+            this.window7.Size = new System.Drawing.Size(75, 70);
+            this.window7.TabIndex = 6;
+            this.window7.Text = "7";
+            this.window7.UseVisualStyleBackColor = true;
+            this.window7.Click += new System.EventHandler(this.Mark);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(393, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(10, 10);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 277);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.window8);
+            this.Controls.Add(this.window9);
+            this.Controls.Add(this.window7);
+            this.Controls.Add(this.window5);
+            this.Controls.Add(this.window6);
+            this.Controls.Add(this.window4);
+            this.Controls.Add(this.window2);
+            this.Controls.Add(this.window3);
+            this.Controls.Add(this.window1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button window1;
+        private System.Windows.Forms.Button window3;
+        private System.Windows.Forms.Button window2;
+        private System.Windows.Forms.Button window5;
+        private System.Windows.Forms.Button window6;
+        private System.Windows.Forms.Button window4;
+        private System.Windows.Forms.Button window8;
+        private System.Windows.Forms.Button window9;
+        private System.Windows.Forms.Button window7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Windows Form XO/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    
+    public partial class Form1 : Form
+    {
+        bool Result = false ;  
+        int WhoPlay;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Mark(object sender, EventArgs e)
+        {
+            Form1 f = new Form1();
+            Button button = (Button) sender;
+            if (button.Text != "X" && button.Text != "O")
+            {
+                if (WhoPlay % 2 == 0)
+                    button.Text = "X";
+                else
+                    button.Text = "O";
+                if (WhoPlay >= 4)
+                    button1.PerformClick();
+                WhoPlay++;
+              
+            }
+            else
+                MessageBox.Show("Erro");
+            
+               
+                
+
+
+        }
+
+         private void button1_Click(object sender, EventArgs e)
+         {
+               Form1 f = new Form1();
+            Result = false;
+                //רוחב
+            if (window1.Text == window2.Text && window1.Text == window3.Text)
+                Result = true;
+            else if (window4.Text == window5.Text && window4.Text == window6.Text)
+                Result = true;
+            else if (window7.Text == window8.Text && window7.Text == window9.Text)
+                Result = true;
+
+                //אורך
+            else if (window1.Text == window4.Text && window1.Text == window7.Text)
+                Result = true;
+            else if (window2.Text == window5.Text && window2.Text == window8.Text)
+                Result = true;
+            else if (window3.Text == window6.Text && window3.Text == window9.Text)
+                Result = true;
+
+                //אלכסונים
+            else if (window1.Text == window5.Text && window1.Text == window9.Text)
+                Result = true;
+            else if (window3.Text == window5.Text && window3.Text == window7.Text)
+                Result = true;
+
+            if (Result)
+            {
+                if (WhoPlay % 2 == 0)
+                    MessageBox.Show("X Win");
+                else
+                    MessageBox.Show("O Win");
+
+                Application.Exit();
+            }
+         }
+    }
+}
